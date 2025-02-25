501fa59873a3cf44978e925ae13f4cac72fd24b0
sans <sans@rezi.io>
2025-02-25 13:20:00 +0900

    site config update (wip)

--- site.config.ts
+++ site.config.ts
@@ -2,27 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'ec6f013522a4404f86bd4f806cd88648',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
-
+  name: 'sansverse',
+  domain: 'sansverse.co',
+  author: 'Sans Bhatia',
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: "Sans Bhatia's portfolio & blog",
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  github: 'raspberrysans',
+  linkedin: 'sans-bhatia-41818a212',
+  newsletter: 'https://medium.com/@sansverse', // optional newsletter URL
+  youtube: 'https://www.youtube.com/@sansxverse', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -50,16 +47,20 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '7627a01e299d4d3fb5f466dd7cd4de24'
+    },
+    {
+      title: 'Personal Blog',
+      pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+    },
+    {
+      title: 'Projects',
+      pageId: ''
+    }
+  ]
 })
